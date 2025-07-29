d8f051d021cc485e98e26e11fa014d752631a44e
jabsraighs <204bouaki.arthur@gmail.com>
2025-07-30 00:59:42 +0200

    mep du projet e-commerce

---
+++ front/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-gray-900 min-h-screen"> 
+      <Header />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/Button";
+import { ShoppingCart, Search, Menu,  } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
+     
+    </header>
+   
+  );
+};
+
+export default Header;
